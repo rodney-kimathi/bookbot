30954e1723bf5c77c8f655c591fd047d2a110a9c
Rodney Kimathi <rodneykimathi@gmail.com>
2024-03-19 18:57:06 +0300

    Count letters

--- main.py
+++ main.py
@@ -5,11 +5,26 @@ def main():
     word_count = count_words(book)
     print("Number of words:", word_count)
 
+    character_count = count_characters(book)
+    print("Number of characters:", character_count)
+
 def get_book(path):
     with open(path) as book:
         return book.read()
     
 def count_words(text):
     return len(text.split())
+
+def count_characters(text):
+    characters = {}
+
+    for character in text:
+        if not character.isalpha():
+            continue
+
+        character = character.lower()
+        characters[character] = characters[character] + 1 if character in characters else 1
+
+    return characters
 
 main()
